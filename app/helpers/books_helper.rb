0151e68f1e7a3f5e03af4cdcc9d7e4498fae119f
Pablo <pablo.moreiramora@gmail.com>
2011-04-09 22:43:37 -0300

    Fix author filter

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -10,6 +10,7 @@ module BooksHelper
 
   def load_authors
   	options = []
+    options << content_tag(:option, "Author", :rel => collection_path)
     Book.all.map(&:author).uniq.each do |a|
     	options << content_tag(:option, a, :rel => collection_path(:author => a))
     end
