d5bc6815658de897e54313d179d157cfd7a43dfd
Pablo <pablo.moreiramora@gmail.com>
2011-07-31 21:21:17 -0300

    Fixed search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,26 @@
 class BooksController < InheritedResources::Base
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
+  before_filter :build_search
 
-	def create
-  	create! do |success, failure|
+  def create
+    create! do |success, failure|
       success.html { redirect_to collection_path }
     end
   end
 
-	def update
-  	update! do |success, failure|
+  def update
+    update! do |success, failure|
       success.html { redirect_to collection_path }
     end
   end
 
   protected
-        
+
   def collection
+    build_search
+  end
+
+  def build_search
     @search = Book.search(params[:search])
     @books = @search.all.paginate(:page => params[:page]) if params[:search]
     if params[:author]
@@ -23,5 +28,5 @@ class BooksController < InheritedResources::Base
     else
       @books ||= end_of_association_chain.paginate(:page => params[:page])
     end
-  end 
+  end
 end
